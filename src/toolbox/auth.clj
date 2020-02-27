f727d4fed60161b01fd1ae61daa791ae4a21c26b
Anton Chebotaev <anton.chebotaev@gmail.com>
2020-02-27 02:32:24 +0100

    make host configurable for deployments

--- src/toolbox/auth.clj
+++ src/toolbox/auth.clj
@@ -3,6 +3,7 @@
             [geheimtur.util.auth :as geheimtur]
             [wrench.core :as cfg]))
 
+(cfg/def host {:default "http://localhost:8080"})
 (cfg/def client-id {:require true})
 (cfg/def client-secret {:require true})
 
@@ -17,7 +18,7 @@
   {:google {:auth-url           "https://accounts.google.com/o/oauth2/auth"
             :client-id          client-id
             :client-secret      client-secret
-            :callback-uri       "http://localhost:8080/oauth.callback"
+            :callback-uri       (str host "/oauth.callback")
             :scope              "profile email"
             :token-url          "https://accounts.google.com/o/oauth2/token"
             :user-info-url      "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
